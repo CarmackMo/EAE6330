c169ccc5f1f250876dbefd3dcb4f0e53d91789cf
WaterFriend <116010169@link.cuhk.edu.cn>
2024-01-18 12:27:57 -0700

    Develop rotator

--- Assets/Scripts/Rotator.cs
+++ Assets/Scripts/Rotator.cs
@@ -10,28 +10,27 @@ public class Rotator : MonoBehaviour
 
 
 
-
-    private void OnMouseDrag()
-    {
-        float mousX = Input.GetAxis("Mouse X");
-        float mousY = Input.GetAxis("Mouse Y");
-        transform.Rotate(mousY * rotationSpeed, -mousX * rotationSpeed, 0, Space.World);
-
-
-        Debug.LogWarning("Get Mouse Button");
-    }
-
-
-
     // Update is called once per frame
     void Update()
     {
         
         if (Input.GetMouseButton(0))
         {
-
+            float mousX = Input.GetAxis("Mouse X");
+            float mousY = Input.GetAxis("Mouse Y");
+            transform.Rotate(mousY * rotationSpeed, -mousX * rotationSpeed, 0, Space.World);
         }
-        
+        else if (Input.GetMouseButtonUp(1)) 
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 500))
+            {
+                Debug.Log("Mouse click hit!");
+            }
+        }
 
 
     }
