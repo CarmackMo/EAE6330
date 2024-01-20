5f0c2eb088475f2ca7a293387da2b081e7ae4719
WaterFriend <116010169@link.cuhk.edu.cn>
2024-01-19 17:05:17 -0700

    Develop MineContainer (develop mine generation and indicator initialization, )

--- Assets/Scripts/MineObject.cs
+++ Assets/Scripts/MineObject.cs
@@ -1,24 +1,43 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class MineObject : MonoBehaviour
 {
-
     [SerializeField] private GameObject m_mine_unchecked = null;
-
     [SerializeField] private GameObject m_mine_labled = null;
-
     [SerializeField] private GameObject m_mine_indicator = null;
 
-    
+    [SerializeField] private Canvas m_canvas = null;
+    [SerializeField] private TextMeshProUGUI m_text = null;
+
+
+    public enum e_MineType { Mine, Normal };
+    public enum e_MineState { UnChecked, Checked, Labeled  };
+
+    public e_MineType Type { get { return m_type; } set { m_type = value; } }
+    [SerializeField] private e_MineType m_type = e_MineType.Normal;
+    public e_MineState State { get { return m_state; } set { m_state = value; } }   
+    private e_MineState m_state = e_MineState.UnChecked;
+
+
+
+
+    private void Start()
+    {
+        m_canvas.worldCamera = Camera.main;
+    }
+
+
+    private void Update()
+    {
+        m_mine_indicator.transform.LookAt(Camera.main.transform);
+    }
 
 
     public void OnRightMouseClick()
     {
-
         m_mine_unchecked.SetActive(false);
-
+        m_mine_indicator.SetActive(true);
     }
 
 
@@ -27,4 +46,18 @@ public class MineObject : MonoBehaviour
         m_mine_unchecked.SetActive(false);
         m_mine_labled.SetActive(true);
     }
+
+
+    public void InitialzieIndicator(int i_mineNear)
+    {
+        m_text.text = i_mineNear.ToString();
+    }
+
+
+    // Test
+    public void SetColor()
+    {
+        m_text.color = Color.red;
+    }
+
 }
