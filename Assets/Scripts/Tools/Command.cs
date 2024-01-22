54d95a07831a143cadb4f059d758bfebe8964a09
WaterFriend <116010169@link.cuhk.edu.cn>
2024-01-21 21:32:52 -0700

    Clean up code

--- Assets/Scripts/Tools/Command.cs
+++ Assets/Scripts/Tools/Command.cs
@@ -9,16 +9,16 @@ public interface ICommand
 
 public enum ECmdType { None, RightMouse, ScrollMouse };
 
+
 class Command<TReceiver> : ICommand where TReceiver : class
 {
+    private ECmdType m_cmdType = ECmdType.None;
+    public ECmdType CmdType {  get { return m_cmdType; } }
+
     private TReceiver m_receiver = null;
     public TReceiver receiver { get { return m_receiver; } }
 
     private Action<TReceiver> m_action = null;
-    public Action<TReceiver> action { get { return m_action; } }
-
-    private ECmdType m_cmdType = ECmdType.None;
-    public ECmdType CmdType {  get { return m_cmdType; } }
 
 
     public Command(TReceiver i_receiver, Action<TReceiver> i_action, ECmdType i_type)
