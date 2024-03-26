73c31812d3d4f163395d82cf170f166094577202
WaterFriend <116010169@link.cuhk.edu.cn>
2024-03-25 20:05:37 -0600

    Add TextMeshPro

--- Assets/Scripts/EventGenerator.cs
+++ Assets/Scripts/EventGenerator.cs
@@ -1,15 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
-
-
-
 
 public class EventGenerator : MonoBehaviour
 {
 
-    float pivot = Random.Range(0.0f, 100.0f);
+    public NormalEventAsset normalEvent = null;
 
 
 
 
 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void GenerateEvent()
+    {
+        float pivot = 0.0f;
+        string playerEvent = "";
+        pivot = Random.Range(0.0f, 100.0f);
+
+        if (pivot < 100.0f)
+        {
+            playerEvent = normalEvent.GetEvent();
+        }
+    }
 }
