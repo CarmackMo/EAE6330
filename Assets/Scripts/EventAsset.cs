23bddcdb4fb469a09888d56a4bd5ca951214e0d7
WaterFriend <116010169@link.cuhk.edu.cn>
2024-03-25 19:51:30 -0600

    Develop scriptable object

---
+++ Assets/Scripts/EventAsset.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+[CreateAssetMenu(fileName = "EventAssets", menuName = "ScriptableObject/EventAssets")]
+public class EventAsset : ScriptableObject
+{
+
+    // Data
+    //=========================
+
+    [Serializable]
+    public struct EventConfig
+    {
+        public string background;
+
+        public List<string> goodResultList;
+
+        public List<string> badResultList;
+    }
+
+    [SerializeField]
+    private List<EventConfig> eventList = new List<EventConfig>();
+
+
+
+    // Implementation
+    //=========================
+
+    public string GetEvent(bool status)
+    {
+        int idx = 0;
+        string res = "";
+
+        idx = UnityEngine.Random.Range(0, eventList.Count);
+        EventConfig config = eventList[idx];
+
+        res += config.background;
+
+        if (status) 
+        {
+            idx = UnityEngine.Random.Range(0, config.goodResultList.Count);
+            if (config.goodResultList.Count > 0)
+                res += config.goodResultList[idx];
+        }
+        else
+        {
+            idx = UnityEngine.Random.Range(0, config.badResultList.Count);
+            if (config.badResultList.Count > 0)
+                res += config.badResultList[idx];
+        }
+
+        return res;
+    }
+
+}
---
+++ Assets/Scripts/EventGenerator.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+
+
+
+public class EventGenerator : MonoBehaviour
+{
+
+    float pivot = Random.Range(0.0f, 100.0f);
+
+
+
+
+
+}
